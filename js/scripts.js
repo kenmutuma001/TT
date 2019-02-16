158e903e2b84a8cc75b16d700b487de0cb9b4a0f
santa <santa@northpole.com>
2019-02-16 13:41:49 +0300

    link javascript to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,1 @@
-function track(a,b,c) {
-  a = parseInt(prompt ('Enter side a:'));
-  b = parseInt(prompt ('Enter side b:'));
-  c = parseInt(prompt ('Enter side c:'));
-  total= a+b+c
-  alert (total)
-}
+
