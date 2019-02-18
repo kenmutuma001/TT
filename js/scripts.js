f1da7c5cffa7800e50a6c30f2ff004f72caac92f
santa <santa@northpole.com>
2019-02-18 06:39:58 +0300

    define function tracker

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
+function tracker(){
+  var sideA=parseInt(document.getElementById("a").value);
+  var sideB=parseInt(document.getElementById("b").value);
+  var sideC=parseInt(document.getElementById("c").value);
+  var ans = document.getElementById('alert');
+  var no = document.getElementById("no");
+  var one = sideA + sideB;
+  var two = sideA + sideC;
+  var three = sideB + sideC;
 
+  if(one <= sideC || two <= sideB || three <= sideA) {
+    ans.innerHTML="NOT A TRIANGLE";
+    no.hide(1000)
+  }
+  else if(sideA===sideB && sideB===sideC){
+    ans.innerHTML="AN EQUILATERAL TRIANGLE";
+  }
+  else if(sideA===sideB || sideA===sideC || sideB === sideC){
+    ans.innerHTML="AN ISSOCELES TRIANGLE";
+  }
+  else if(sideA != sideB && sideB != sideC){
+    ans.innerHTML="A SCALENE TRIANGLE";
+  };
+};
